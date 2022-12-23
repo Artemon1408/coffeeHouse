3dbe6d0371aa5d0cf4b40432da806a2460e74080
Artem <12artemondrozhzhyn@gmail.com>
2022-12-23 15:41:55 +0200

    v.01

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,17 @@
+import AboutCoffeePage from "../pages/aboutCoffe/AboutCoffee";
+import CoffeePage from "../pages/coffeePage/CoffeePage";
+import MainPage from "../pages/mainPage/MainPage";
+import PleasurePage from "../pages/pleasurePage/PleasurePage";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      {/* <MainPage /> */}
+      {/* <CoffeePage /> */}
+      {/* <AboutCoffeePage /> */}
+      <PleasurePage />
+    </>
+  );
 }
 
 export default App;
